d0a41b0e27855012bf3a9b185ace93672d5858ce
Amritpal Singh <AmritMaan2221@gmail.com>
2022-02-06 12:04:23 -0500

    editing page heading message

--- ClientApp/src/components/PageHeading/PageHeading.js
+++ ClientApp/src/components/PageHeading/PageHeading.js
@@ -15,9 +15,8 @@ function PageHeading() {
         <div className="page-header">
             <h2 className="page-header__heading">Water the plants</h2>
             <p>
-                You can <em>individually</em> fill the water by going on each plant or
-                you can choose to water <em>multiple plants</em>  together by scrolling at the end of the website.
-                If the plants aren't watered for more than <em>six hours</em> you are notified with red alert on that plant.
+                You can <em>individually</em> fill the water by going on each plant.
+                If the plants aren't watered for more than <em>six hours</em> you are notified with red alert error msg and green background color on that plant.
             </p>
         </div>
     );
